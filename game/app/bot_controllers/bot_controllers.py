d30dffe1ae7de42130a3acc123f1e1bf2b2017e4
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-12-08 02:36:11 +0200

    Fixed bot range of interaction with player

--- game/app/bot_controllers/bot_controllers.py
+++ game/app/bot_controllers/bot_controllers.py
@@ -4,30 +4,27 @@ from app.controllers.attack import MagicController
 from app import consts
 import random
 
+
 class BotMoveController(MoveController):
     def __init__(self, bot, player, default_state=consts.IDLE, speed=1):
         super().__init__(bot, default_state, pg.K_a, speed)
         self._player = player
 
     def move(self):
-        if not self._player:
+        if not self._player.alive():
+            self._state = consts.IDLE
             return
 
         bot_pos = pg.math.Vector2(self._owner.rect.center)
         player_pos = pg.math.Vector2(self._player.rect.center)
 
         direction = player_pos - bot_pos
-        distance = direction.length()
 
         if direction.x == 0 and direction.y == 0:
             return
-
-        if distance < 200:
+        else:
             direction.normalize_ip()
             self._state = consts.RUN
-        else:
-            direction = pg.math.Vector2(0, 0)
-            self._state = consts.IDLE
 
         self._side = pg.K_a if direction.x < 0 else pg.K_d
 
@@ -37,22 +34,29 @@ class BotMoveController(MoveController):
 
 
 class BotMagicController(MagicController):
-    def __init__(self, owner, player, cooldown=60):
+    def __init__(self, owner, player, cooldown=300):
         super().__init__(owner)
         self._player = player
         self._cooldown = cooldown
         self._last_attack = 0
 
     def attack(self):
-        if not self._player:
+        if not self._player.alive():
             return
 
         bot_pos = pg.math.Vector2(self._owner.rect.center)
         player_pos = pg.math.Vector2(self._player.rect.center)
         distance = bot_pos.distance_to(player_pos)
 
-        if distance < 150 and pg.time.get_ticks() - self._last_attack > self._cooldown:
-            self._last_attack = pg.time.get_ticks()
+        self._last_attack = pg.time.get_ticks()
 
-            self._state = random.choice([consts.ATTACK1, consts.ATTACK2, consts.ATTACK3])
-            self._attack_event(self._state, player_pos)
+        if pg.time.get_ticks() - self._last_attack > self._cooldown:
+            self._state = consts.RUN
+            return
+
+        if distance < 150:
+            self._state = random.choice([consts.ATTACK2, consts.ATTACK3])
+        else:
+            self._state = consts.ATTACK1
+
+        self._attack_event(self._state, player_pos)
